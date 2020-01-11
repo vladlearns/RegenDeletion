68d8f3d7e1a1f7b47b3fa43e7a4badf6fb13148b
vladthedev <vladyslav.thedev@gmail.com>
2020-01-11 12:47:46 +0200

    First commit

---
+++ RegenDeletion.py
@@ -0,0 +1,25 @@
+#The main intention of the script is the ability to remove a file, that can regenerate itself. Later you can put it in the task manager.
+# Made by vladthedev in 2020.
+
+
+import os
+import subprocess
+
+#I have made it flexible, so you can easily change the path to the file and app name.
+initFile = 'PATH';
+#Has to be type string, so we would encode it later in order to check if it is a running task.
+yourApp = "yourapp";
+#Getting list of processes.
+s = subprocess.check_output('tasklist', shell=True)
+#Cheking if the process isn't in the processes list.
+if yourApp.encode() not in s:
+#Removing a file.
+    if os.path.exists(initFile):
+            os.remove(initFile)
+            print("File has been Removed!")
+# Throwing an error if there is no file in the path.
+    else:
+        print(FileNotFoundError)
+else:
+    print("Running!")
+
