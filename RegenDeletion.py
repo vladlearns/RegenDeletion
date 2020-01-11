2c6233f6385270e98e4290f2e2157127a78aa8cd
vladthedev <vladyslav.thedev@gmail.com>
2020-01-11 18:19:00 +0200

    DoubleBrAdded

--- RegenDeletion.py
+++ RegenDeletion.py
@@ -6,11 +6,11 @@ import os
 import subprocess
 
 #I have made it flexible, so you can easily change the path to the file and app name.
-initFile = 'PATH';
+initFile = "PATH";
 #Has to be type string, so we would encode it later in order to check if it is a running task.
 yourApp = "yourapp";
 #Getting list of processes.
-s = subprocess.check_output('tasklist', shell=True)
+s = subprocess.check_output("tasklist", shell=True)
 #Cheking if the process isn't in the processes list.
 if yourApp.encode() not in s:
 #Removing a file.
